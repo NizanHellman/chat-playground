4be10cfed22a2193673ce72a954ebea0e03f6110
NizanHellman <nizan.hellman@emedgene.com>
2024-04-08 14:43:32 +0300

    align

---
+++ templates/play.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <div v-for="message in messages" :key="message.id">
+      {{ message.text }}
+    </div>
+    <input v-model="newMessage" @keyup.enter="sendMessage" placeholder="Type your message here...">
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      messages: [],
+      newMessage: ''
+    }
+  },
+  methods: {
+    sendMessage() {
+      if (!this.newMessage) {
+        return
+      }
+      this.messages.push({
+        id: Date.now(),
+        text: this.newMessage
+      })
+      this.newMessage = ''
+    }
+  }
+}
+</script>
+
+
